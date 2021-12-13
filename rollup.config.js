29a9364c3e10e95fd41638293819890bbe0ac955
Aria Buckles <aria@toole1.com>
2021-12-12 20:05:15 -0800

    rollup working

--- rollup.config.js
+++ rollup.config.js
@@ -2,12 +2,20 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import babel from '@rollup/plugin-babel';
 import json from '@rollup/plugin-json';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 export default {
   input: 'src/perseus.js',
+  external: (path) => !path.startsWith('.') && !path.startsWith('/'),
   output: {
     file: 'build/perseus.cjs',
     format: 'cjs',
   },
-  plugins: [json(), resolve(), commonjs(), babel({ babelHelpers: 'bundled' })]
+  plugins: [
+    json(),
+    resolve(),
+    commonjs(),
+    babel({ babelHelpers: 'bundled' }),
+    visualizer({ filename: 'build/rollup-stats.html' }),
+  ],
 };
