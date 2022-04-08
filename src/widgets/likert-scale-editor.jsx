dff7a4975ca44ecb350261400a3dbeab3d02c9f3
Aria Buckles <aria@toole1.com>
2022-04-07 18:23:37 -0700

    Likert Scale: Add text labels

--- src/widgets/likert-scale-editor.jsx
+++ src/widgets/likert-scale-editor.jsx
@@ -1,8 +1,27 @@
 import * as React from "react";
-import LikertScale from './likert-scale.jsx';
+import PropTypes from "prop-types";
+import { StyleSheet, css } from "aphrodite";
+import { LikertFaces } from './likert-scale.jsx';
+
+const styles = StyleSheet.create({
+    face: {
+        display: 'inline-block',
+        width: 30,
+        height: 30,
+        margin: 5,
+        verticalAlign: 'middle',
+    },
+});
 
 class LikertScaleEditor extends React.Component {
-    static propTypes = { };
+    static propTypes = {
+        labels: PropTypes.arrayOf(PropTypes.string),
+        onChange: PropTypes.func.isRequired,
+    };
+
+    static defaultProps = {
+        labels: ["", "", "", "", ""],
+    };
 
     constructor(props) {
         super(props)
@@ -10,17 +29,26 @@ class LikertScaleEditor extends React.Component {
 
     render() {
         return <div>
-            <div>
-            The likert scale submits a score of 1 (☹️) to 5 (😀).
-            </div>
-            <div>
-            (There are no configuration options currently.)
-            </div>
+            Choice Labels:
+            {this.props.labels.map((label, i) => (
+                <div>
+                    <img className={css(styles.face)} src={LikertFaces[i]} alt="" />
+                    <input type="text" className={css(styles.labelInput)} value={label} onChange={(e) => this._changeLabel(i, e.target.value)} />
+                </div>
+            ))}
         </div>
     }
 
+    _changeLabel = (index, newLabel) => {
+        this.props.onChange({
+            labels: this.props.labels.map((label, i) => i === index ? newLabel : label),
+        });
+    };
+
     serialize() {
-        return { };
+        return {
+            labels: this.props.labels,
+        };
     }
 }
 
